c5a002a15565cfb8d110dbac6cd0f5f639cab1a9
Nishant Shinde <nishant.impetrosys@gmail.com>
2021-07-03 17:29:32 +0530

    Initial Commit

---
+++ swift_demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift_demo
+//
+//  Created by mac on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
